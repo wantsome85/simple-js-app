ceaa5564dc4e4d8c92e7c95242add11c31cad04f
wantsome85 <77009800+wantsome85@users.noreply.github.com>
2021-02-24 13:22:31 -0600

    Added pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,51 @@
-alert('Hello world');
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['Grass', 'Poison']
+  },
+  {
+    name: 'Charmander',
+    height: 0.6,
+    type: ['Fire']
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    type: ['Water']
+  },
+  {
+    name: "Dragonite",
+    height: 2.2,
+    type: ['Dragon', 'Flying']
+  },
+  {
+    name: "Mewtwo",
+    height: 2,
+    type: ['Psychic']
+  },
+  {
+    name: "Mew",
+    height: 0.4,
+    type: ['Psychic']
+  },
+  {
+    name: "Gengar",
+    height: 1.5,
+    type: ["Ghost" , "Poison"]
+  }
+];
 
-let favoriteFood = 'Ramen';
-document.write(favoriteFood);
-favoriteFood = 'Noodle';
-document.write(favoriteFood);
+for (var i = 0; i < pokemonList.length; i++) {
+  //prints pokemon name and height
+  let name = pokemonList[i].name;
+  let height = pokemonList[i].height;
+
+  document.write(`<p id ="pokemon"> ${name} height: ${height} `);
+
+  //comments on pokemon height
+    if(height > 1.0){
+      document.write(' - that\'s pretty tall') + '</p>';
+    }
+
+}
